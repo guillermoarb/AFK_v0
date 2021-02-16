02db3ff9c9ac1ada20ab33308d655b2753430488
Rodriguez Guillermo (UIDN6202) <guillermo.2.rodriguez@continental-corporation.com>
2021-02-16 17:02:27 -0600

    Added report feature

--- load_ui.py
+++ load_ui.py
@@ -6,6 +6,8 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 
+
+
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
@@ -24,12 +26,13 @@ class Window(QWidget):
         # self.label.setGeometry(500,0,16,16)
 
     def show_dialog(self):
+
         dialog = Dialog(self)  # self hace referencia al padre
         dialog.ui.show()
 
     def toggle_timer(self):
         if timer_1s.isActive() == True:
-            file_log()
+            update_report()
             timer_1s.stop()
             self.ui.counter_lb.setStyleSheet("color:#BF616A;")
         else:
@@ -39,6 +42,7 @@ class Window(QWidget):
 
 
 class Dialog(QDialog):
+
     def __init__(self, *args, **kwargs):
         super(Dialog, self).__init__(*args, **kwargs)
         self.ui = QUiLoader().load(QFile("ui/dialog form.ui"))
@@ -53,18 +57,38 @@ class Dialog(QDialog):
         self.ui.task_le.setText("")
 
     def ok_clicked(self):
-        app_window.ui.task_lb.setText( self.ui.task_le.text() )
+        global activity_log
+        global activity_name
+        global activity_time
+
+
+        #Update report information
+        update_report()
+
+        activity_log = self.ui.task_le.text()
+        app_window.ui.task_lb.setText( activity_log)
         #app_window.ui.project_lb.setText( self.ui.project_le.text() )
-        app_window.ui.ticket_lb.setText(self.ui.ticket_le.text())
+        activity_name = self.ui.ticket_le.text()
+        app_window.ui.ticket_lb.setText(activity_name)
+
+        #Clear the activity timer, this is a new activity
+        activity_time = activity_time.replace(0,0,0)
+
+        #Update report information
+        update_report()
+
         self.ui.hide()
         
 
     def cancel_clicked(self):
         self.ui.hide()
 
-def file_log():
-    with open("Report.txt", "a") as file_object:
-        file_object.write("Time: {} \tTask: {}\n".format(app_window.ui.counter_lb.text(), app_window.ui.task_lb.text() ))
+def update_report():
+
+    report_file_name = today.strftime("%m%d%y") + "_AFK.txt"
+
+    with open(report_file_name, "a") as file_object:
+        file_object.write("{} {} Activity:{} \t\tLog:{}\n".format(today.strftime("%m/%d/%y"),app_window.ui.counter_lb.text(), app_window.ui.ticket_lb.text(), app_window.ui.task_lb.text() ))
 
 def clickable(widget):
     class Filter(QObject):
@@ -84,87 +108,107 @@ def clickable(widget):
 
 
 def task_1s():
-    
-    global loop_time_sec
+    global timer_report_cnt
 
+    #Update the report timer
+    timer_report_cnt = timer_report_cnt + 1
+    #Every 15 min
+    if(timer_report_cnt >= (60 * 15)):
+        update_report()
+        timer_report_cnt = 0
+
+    update_log_timers()
+
+    #Update log ON time
+    app_window.ui.counter_lb.setText(time_log_on.strftime("%H:%M:%S"))
+
+    #Update activity time
+    app_window.ui.task_lb.setText("{} {}".format(activity_log, activity_time.strftime("%H:%M:%S")))
+
+
+def update_log_timers():
+    global time_log_on
+    global loop_time_sec
+    global activity_time
+    global time_log_off
+    global pause_time
+
+    #Check if screen is not locked
+    if (get_lock_status() == False):
+        #Update general log ON timer
+        updated_clock = clock_add_time_sec(time_log_on, loop_time_sec)
+        time_log_on = time_log_on.replace(updated_clock.hour, updated_clock.minute, updated_clock.second)
+        #update the activity timer
+        updated_clock = clock_add_time_sec(activity_time, loop_time_sec)
+        activity_time = time_log_on.replace(updated_clock.hour, updated_clock.minute, updated_clock.second)
+    else:
+        #Update general log OFF timer
+        updated_clock = clock_add_time_sec(time_log_off, loop_time_sec)
+        time_log_off = time_log_off.replace(updated_clock.hour, updated_clock.minute, updated_clock.second)
+        #update the pause timer
+        updated_clock = clock_add_time_sec(pause_time, loop_time_sec)
+        pause_time = pause_time.replace(updated_clock.hour, updated_clock.minute, updated_clock.second)
+
+
+
+def get_lock_status():
     #Get foreground window ID
     foreground_window = user32.GetForegroundWindow() 
     #Get foreground window text 
     window_text_len = user32.GetWindowTextLengthW(foreground_window)
     window_text = ctypes.create_unicode_buffer(window_text_len + 1)
+    
     user32.GetWindowTextW(foreground_window, window_text, window_text_len + 1)
-
     #Evaluate if the PC is locked and then increase the right timer
-    if (foreground_window == 0)  or (window_text.value == "Windows Default Lock Screen"):
-        time_log_on, time_log_off = clock_add_sec("log_off", loop_time_sec)
+    if ((foreground_window == 0) or (window_text.value == "Windows Default Lock Screen")):
+        lock_status = True
     else:
-        time_log_on , time_log_off = clock_add_sec("log_on", loop_time_sec)
+        lock_status = False
+
+    return lock_status
 
 
+def clock_add_time_sec(clock_in, plus_seconds):
 
-    #time_log_on = str(time_logged_on_hr) + ":" + str(time_logged_on_min) + "m : " + str(time_logged_on_sec) + "s"
-    #time_log_off = "LOG OFF: " + str(time_logged_off_hr) + "h : " + str(time_logged_off_min) + "m : " + str(time_logged_off_sec) + "s"
+    seconds = clock_in.second
+    minutes = clock_in.minute
+    hours = clock_in.hour
 
-    app_window.ui.counter_lb.setText(time_log_on.strftime("%H:%M:%S"))
+    seconds = seconds + plus_seconds
 
-    # app_window.ui.counter_lb.setText(time_log_on.strftime("%H:%M:%S"))
-    # app_window.ui.counter_lb.repaint()
-    # app_window.ui.time_cnt_lbl_log_off.setText(time_log_off.strftime("%H:%M:%S"))
-    # app_window.ui.time_cnt_lbl_log_off.repaint()
+    if (seconds >= 60):
+        minutes = minutes + 1
+        seconds = 0
 
-def clock_add_sec(clock, plus_seconds):
+    if (minutes >= 60):
+            hours = hours + 1
+            minutes = 0
 
-    global time_logged_on_sec
-    global time_logged_on_min
-    global time_logged_on_hr
+    clock_out = datetime.time(hours,minutes,seconds)
 
-    global time_logged_off_sec
-    global time_logged_off_min
-    global time_logged_off_hr
+    return clock_out
 
-    time_log_on = datetime.time()
-    time_log_off = datetime.time()
-
-    if(clock == "log_on"):
-        
-        time_logged_on_sec = time_logged_on_sec + plus_seconds
-
-        if (time_logged_on_sec == 60):
-            time_logged_on_min = time_logged_on_min + 1
-            time_logged_on_sec = 0
-
-        if (time_logged_on_min == 60):
-            time_logged_on_hr = time_logged_on_hr + 1
-            time_logged_on_min = 0
-
-        
-
-    if(clock == "log_off"):
-
-        time_logged_off_sec = time_logged_off_sec + plus_seconds
-
-        if (time_logged_off_sec == 60):
-            time_logged_off_min = time_logged_off_min + 1
-            time_logged_off_sec = 0
-
-        if (time_logged_off_min == 60):
-            time_logged_off_hr = time_logged_off_hr + 1
-            time_logged_off_min = 0
-
-    time_log_on = datetime.time(time_logged_on_hr,time_logged_on_min,time_logged_on_sec)
-    time_log_off = datetime.time(time_logged_off_hr,time_logged_off_min,time_logged_off_sec)
-
-    return (time_log_on , time_log_off)
 
 
 if __name__ == '__main__':
     #Init the time counters
+    time_log_on = datetime.time()
+    time_log_off = datetime.time()
+    activity_time = datetime.time()
+    pause_time = datetime.time()
+    today = datetime.datetime.now()
+
+    timer_report_cnt = 0
+
     time_logged_on_sec = 0
     time_logged_on_min = 0
     time_logged_on_hr = 0
     time_logged_off_sec = 0
     time_logged_off_min = 0
     time_logged_off_hr = 0
+
+    activity_log = "Init"
+    activity_name = "Init"
 
     loop_time_sec = 1
 
